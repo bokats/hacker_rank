2362fee78e410708880a725b15a9d8239ccf1576
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-05-12 21:11:39 -0700

    Add letter phone digit combos

---
+++ letter_combinations.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        numbers_hash = {'2': ['a', 'b','c'], '3': ['d','e','f'], '4': ['g','h','i'], \
+        '5': ['j','k','l'], '6': ['m','n','o'], '7': ['p','q','r','s'], '8': ['t','u','v'], \
+        '9': ['w','x', 'y', 'z']}
+
+        final_result = []
+
+        for digit in digits:
+            temp = []
+            for letter in numbers_hash[digit]:
+                if len(final_result) < 1:
+                    temp.append(letter)
+                else:
+                    for combo in final_result:
+                        temp.append(combo + letter)
+
+            final_result = temp
+
+        return final_result
