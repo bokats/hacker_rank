a7e2c31c8e3c967a73d48f66659cfe0a69a98a83
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-05-20 10:36:53 -0700

    Complete merge

--- merge_intervals.rb
+++ merge_intervals.rb
@@ -1,13 +1,19 @@
 def merge(intervals)
 
   result = []
+  sorted_intervals = intervals.sort_by { |array| array[0] }
 
-  intervals.sort_by { |array| array[0] }.each do |subarray|
+  sorted_intervals.each do |subarray|
     if result.empty?
       result << subarray
+    elsif subarray[0] <= result[-1][1]
+      result[-1][1] = subarray[1]
     else
-      
+      result << subarray
     end
   end
-
+  result
 end
+
+p merge([[1,3]])
+p merge([[1,3], [2,4], [5,8]])
