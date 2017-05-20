aeffa27b0f5e6e50acf275e75331988061d93275
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-05-19 21:26:26 -0700

    Add merge intervals

---
+++ merge_intervals.rb
@@ -0,0 +1,13 @@
+def merge(intervals)
+
+  result = []
+
+  intervals.sort_by { |array| array[0] }.each do |subarray|
+    if result.empty?
+      result << subarray
+    else
+      
+    end
+  end
+
+end
