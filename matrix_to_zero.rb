c8160f1819216f2881bc93c34378d2e1b727e43e
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-05-21 18:50:02 -0700

    Add sort colors

--- matrix_to_zero.rb
+++ matrix_to_zero.rb
@@ -1,3 +1,35 @@
+require 'set'
+require 'byebug'
+
 def set_zeroes(matrix)
-    
+
+  changed = Set.new
+  matrix.each_index do |row|
+    found_zero = false
+    0.upto(matrix[row].length - 1) do |column|
+      if matrix[row][column] == 0 && !changed.include?([row, column])
+        matrix.each_index do |row_idx|
+          if matrix[row_idx][column] != 0
+            if row_idx > row
+              changed << [row_idx, column]
+            end
+            matrix[row_idx][column] = 0
+          end
+        end
+        found_zero = true
+      end
+    end
+    if found_zero
+      matrix[row] = Array.new(matrix[row].length) { 0 }
+    end
+  end
 end
+
+m = [[0,0,0,5],
+     [4,3,1,4],
+     [0,1,1,4],
+     [1,2,1,3],
+     [0,0,1,1]]
+
+set_zeroes(m)
+p m
---
+++ sort_colors.rb
@@ -0,0 +1,8 @@
+def sort_colors(nums)
+  zero_idx = nil
+  one_idx = nil
+  two_idx = nil
+  nums.each_index do |idx|
+
+  end
+end
