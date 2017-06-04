69df73e5042a5f33ff81d7578a2da016545b3078
Bo Katsarov <bozhidar.katsarov@gmail.com>
2017-06-03 17:34:42 -0700

    Add js problem

---
+++ word_ladder.js
@@ -0,0 +1,28 @@
+// var ladderLength = function(beginWord, endWord, wordList) {
+//   let steps = 1;
+//   let wordList_set = new Set([]);
+//   for (let i = 0; i < wordList.length; i++) {
+//     wordList_set.add(wordList[i]);
+//   }
+//
+//   let testWord;
+//   while (beginWord !== endWord) {
+//     for (let i = 0; i < beginWord.length; i++) {
+//       testWord = beginWord.slice(0,i) + endWord[i] + beginWord.slice(i + 1, beginWord.length);
+//       if (wordList_set.includes(testWord)) {
+//         beginWord = testWord;
+//         wordList_set.delete(testWord);
+//         steps ++;
+//         break;
+//       } else {
+//
+//       }
+//     }
+//   }
+// };
+
+// ladderLength('hit', 'cog',["hot","dot","dog","lot","log","cog"]);
+
+for (let i = 0; i < 5; i++) {
+  
+}
